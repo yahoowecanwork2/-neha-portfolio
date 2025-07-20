4025c070f4ffc086c6bc609ffc0402f6d3f2f5b2
Neha Yadav <ny663922@gmail.com>
2025-07-20 21:48:16 +0530

    added more project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,6 +14,24 @@ function Project() {
                     <p>An e-commerce fashion website built using HTML and CSS.</p>
                     <a href="https://github.com/yahoowecanwork2/hunnyBee" target="_blank" rel="noreferrer">View Code</a>
                 </div>
+                <div className="project-card">
+                    {/* <img src={portfolioImg} alt="Portfolio" /> */}
+                    <h3>React Portfolio</h3>
+                    <p>Personal portfolio website made with React and styled with CSS.</p>
+                    <a href="https://github.com/yahoowecanwork2/neha-portfolio" target="_blank" rel="noreferrer">View Code</a>
+                </div>
+                <div className="project-card">
+                    {/* <img src={recipeBookImg} alt="Recipe Book" /> */}
+                    <h3>Recipe Book</h3>
+                    <p>A simple recipe book web app built with React. You can add or view recipe cards.</p>
+                    <a href="https://github.com/yahoowecanwork2" target="_blank" rel="noreferrer">View Code</a>
+                </div>
+                <div className="project-card">
+                    {/* <img src={rpsImg} alt="Rock Paper Scissors" /> */}
+                    <h3>Rock Paper Scissors</h3>
+                    <p>Interactive game built using HTML, CSS, and JavaScript where you play against the computer.</p>
+                    <a href="https://github.com/yahoowecanwork2" target="_blank" rel="noreferrer">View Code</a>
+                </div>
 
             </div>
         </section>
