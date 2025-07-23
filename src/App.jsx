00ba798f121d46790a91b6e613ed7ecc67dcc3b1
Neha Yadav <ny663922@gmail.com>
2025-07-23 20:17:18 +0530

    added router to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router";
 import About from "./components/about";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -6,21 +8,23 @@ import Navbar from "./components/navabar";
 import Project from "./components/Projects";
 import Service from "./components/Service";
 import Skill from "./components/Skills";
-
+import "./App.css";
 
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Service />
-      <Project />
-      <Skill />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Hero />
+        <About />
+        <Service />
+        <Project />
+        <Skill />
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
