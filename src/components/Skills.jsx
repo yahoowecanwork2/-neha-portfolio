90fa6c2b7a819c9ceb9accd3db8f8ccd57ee4c35
Neha Yadav <ny663922@gmail.com>
2025-07-24 01:57:05 +0530

    update skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,41 @@
-import '../components/Skills.css';
 import React from 'react';
-
+import '../components/Skills.css';
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaGithub } from 'react-icons/fa';
 
 function Skill() {
-    const skills = ['HTML5', 'CSS3', 'JavaScript',
-        'React JS', 'Git & GitHub',
-        'Responsive Design', 'VS Code',];
     return (
         <section className="skills" id="skills">
-            <h2>My Skills</h2>
-            <div className="skills-list">
-                {
-                    skills.map((skill, item) => (
-                        <div className="skill-card" key={item}>{skill}</div>
-                    ))
-                }
-            </div>
+            <h2 className="section-title">My Skills 💡</h2>
+            <div className="skills-container">
 
+                <div className="skill-card">
+                    <FaHtml5 className="skill-icon html" />
+                    <p>HTML5</p>
+                </div>
+
+                <div className="skill-card">
+                    <FaCss3Alt className="skill-icon css" />
+                    <p>CSS3</p>
+                </div>
+
+                <div className="skill-card">
+                    <FaJsSquare className="skill-icon js" />
+                    <p>JavaScript</p>
+                </div>
+
+                <div className="skill-card">
+                    <FaReact className="skill-icon react" />
+                    <p>React</p>
+                </div>
+
+                <div className="skill-card">
+                    <FaGithub className="skill-icon github" />
+                    <p>GitHub</p>
+                </div>
+
+            </div>
         </section>
-    )
+    );
 }
 
 export default Skill;
